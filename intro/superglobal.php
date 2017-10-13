147a04e027d62e6bac9505bd5f2c4e5420aa336e
phpacad <git@php-academy.kiev.ua>
2017-10-13 19:06:36 +0300

    Added GET/POST demonstration

---
+++ intro/superglobal.php
@@ -0,0 +1,12 @@
+<pre>
+
+<?php var_dump($_GET, $_POST); ?>
+
+</pre>
+
+
+<form method="post" action="http://26-09.samples.local/intro/superglobal.php?doAuth=1">
+    <input name="login" type="email">
+    <input name="pass" type="password">
+    <input type="submit" value="Submit">
+</form>
